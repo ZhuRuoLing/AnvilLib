eb8589113bb21b4b4a08b0bfff31f54a47f61e7d
ZhuRuoLing <jkl-963@live.com>
2024-09-04 13:07:24 +0800

    🐛 修复模型生成位置不正确

--- common/src/main/java/dev/anvilcraft/lib/data/provider/AnvilLibBlockModelProvider.java
+++ common/src/main/java/dev/anvilcraft/lib/data/provider/AnvilLibBlockModelProvider.java
@@ -1,11 +1,7 @@
 package dev.anvilcraft.lib.data.provider;
 
 import dev.anvilcraft.lib.data.file.BlockModelFile;
-import net.minecraft.data.CachedOutput;
 import net.minecraft.data.PackOutput;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.concurrent.CompletableFuture;
 
 public class AnvilLibBlockModelProvider extends ModelProvider<BlockModelFile> {
 
@@ -15,11 +11,6 @@ public class AnvilLibBlockModelProvider extends ModelProvider<BlockModelFile> {
             PackOutput output
     ) {
         super(BlockModelFile::new, categoryDirectory, modid, output);
-    }
-
-    @Override
-    public @NotNull CompletableFuture<?> run(@NotNull CachedOutput output) {
-        return CompletableFuture.allOf();
     }
 
     @Override
