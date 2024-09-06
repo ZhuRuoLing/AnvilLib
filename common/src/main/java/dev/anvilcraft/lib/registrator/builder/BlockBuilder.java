3b30bfe91ece9ddd10d8c477ddbb74ee069fa698
ZhuRuoLing <jkl-963@live.com>
2024-09-06 23:01:43 +0800

    🐛 删除println

--- common/src/main/java/dev/anvilcraft/lib/registrator/builder/BlockBuilder.java
+++ common/src/main/java/dev/anvilcraft/lib/registrator/builder/BlockBuilder.java
@@ -137,7 +137,6 @@ public class BlockBuilder<T extends Block> extends EntryBuilder<T> {
     @Override
     public BlockEntry<T> register() {
         if (this.recipeFunction != null){
-            System.out.println("register itemBuilder = " + itemBuilder);
             this.itemBuilder.recipe(
                 (itemEntry1, registratorRecipeProvider) -> recipeFunction.accept(itemEntry1, registratorRecipeProvider)
             );
@@ -153,7 +152,6 @@ public class BlockBuilder<T extends Block> extends EntryBuilder<T> {
     }
 
     public BlockBuilder<T> recipe(BiConsumer<ItemEntry<? extends BlockItem>, RegistratorRecipeProvider> fn) {
-        System.out.println("itemBuilder = " + itemBuilder);
         this.recipeFunction = fn;
         return this;
     }
