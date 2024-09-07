610305e68008635633e93788a7b79ebcd8d226fe
Gugle <administration@ztxy666.cn>
2024-09-07 12:44:53 +0800

    🌟 移除 registrator

--- src/main/java/dev/anvilcraft/lib/registrator/entry/EntityEntry.java
+++
@@ -1,7 +0,0 @@
-package dev.anvilcraft.lib.registrator.entry;
-
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
-
-public class EntityEntry<T extends Entity> extends RegistryEntry<EntityType<T>>{
-}
