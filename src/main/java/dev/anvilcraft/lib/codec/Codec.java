15673f7fc89ce595c7ae5cb1e6675aa744650ef8
Gugle <administration@ztxy666.cn>
2024-09-11 03:18:38 +0800

    🌟 创建Codec注解及其处理器

---
+++ src/main/java/dev/anvilcraft/lib/codec/Codec.java
@@ -0,0 +1,11 @@
+package dev.anvilcraft.lib.codec;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Codec {
+}
