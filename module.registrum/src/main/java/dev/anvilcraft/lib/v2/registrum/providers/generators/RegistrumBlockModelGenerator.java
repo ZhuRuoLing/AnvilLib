a4ba375f703160bd3b33d8ae0007ace0769c8792
Gugle <administration@ztxy666.cn>
2026-03-26 02:54:29 +0800

    fix(docs): update original file links in multiple Java files to reflect new directory structure

--- module.registrum/src/main/java/dev/anvilcraft/lib/v2/registrum/providers/generators/RegistrumBlockModelGenerator.java
+++ module.registrum/src/main/java/dev/anvilcraft/lib/v2/registrum/providers/generators/RegistrumBlockModelGenerator.java
@@ -7,7 +7,7 @@
  *  * License, v. 2.0. If a copy of the MPL was not distributed with this
  *  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *  *
- *  * Original File: https://github.com/tterrag1098/Registrate/blob/1.21.5/dev/D:/Projects/repos/AnvilLib/module.registrum/src/main/java/dev/anvilcraft/lib/v2/registrum/providers/generators/RegistrateBlockModelGenerator.java
+ *  * Original File: https://github.com/tterrag1098/Registrate/blob/1.21.5/dev/src/main/java/com/tterrag/registrate/providers/generators/RegistrateBlockModelGenerator.java
  *
  */
 
