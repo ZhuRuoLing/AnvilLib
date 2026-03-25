132e6a1ec9764fd2de3b5054d07e51abb6c47475
Gugle <administration@ztxy666.cn>
2026-03-26 02:39:47 +0800

    refactor(datagen): separate client and server data gathering in AnvilLibDatagen

--- module.recipe/src/main/java/dev/anvilcraft/lib/v2/recipe/data/gen/AnvilLibDatagen.java
+++ module.recipe/src/main/java/dev/anvilcraft/lib/v2/recipe/data/gen/AnvilLibDatagen.java
@@ -6,15 +6,21 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.fml.loading.FMLLoader;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
 @EventBusSubscriber(modid = AnvilLibRecipe.MOD_ID)
 public class AnvilLibDatagen {
     @SubscribeEvent
-    public static void gatherData(GatherDataEvent event) {
+    public static void gatherData(GatherDataEvent.Client event) {
         DataGenerator generator = event.getGenerator();
         PackOutput packOutput = generator.getPackOutput();
-        generator.addProvider(FMLLoader.getDist().isClient(), new ModLanguageProvider(packOutput));
+        generator.addProvider(true, new ModLanguageProvider(packOutput));
+    }
+
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent.Server event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        generator.addProvider(false, new ModLanguageProvider(packOutput));
     }
 }
